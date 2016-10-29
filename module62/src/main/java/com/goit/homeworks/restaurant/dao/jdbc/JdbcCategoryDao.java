7d8ce3d89c3f169899d5bd0199e6531ec328eafe
sevladmain <sevlad.main@gmail.com>
2016-10-29 19:53:11 +0300

    module 62 testing CategoryDao

--- module62/src/main/java/com/goit/homeworks/restaurant/dao/jdbc/JdbcCategoryDao.java
+++ module62/src/main/java/com/goit/homeworks/restaurant/dao/jdbc/JdbcCategoryDao.java
@@ -1,10 +1,13 @@
 package com.goit.homeworks.restaurant.dao.jdbc;
 
 import com.goit.homeworks.restaurant.core.Category;
+import com.goit.homeworks.restaurant.core.Position;
 import com.goit.homeworks.restaurant.dao.CategoryDao;
 import org.apache.log4j.Logger;
 
 import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -31,26 +34,102 @@ public class JdbcCategoryDao implements CategoryDao {
 
     @Override
     public Category create(Category item) {
-        return null;
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("INSERT INTO CATEGORY (NAME)  VALUES (?)",
+                     Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, item.getName());
+            statement.executeUpdate();
+            ResultSet set = statement.getGeneratedKeys();
+            if (set.next()) {
+                item.setId(set.getInt("ID"));
+            } else {
+                LOGGER.error("Unknown Error in create Category: " + item);
+                throw new RuntimeException("Unknown Error in create Category");
+            }
+        } catch (SQLException e) {
+            LOGGER.error("Exception while connecting to DB in method create Category: " + e);
+            throw new RuntimeException(e);
+        }
+        return item;
+
     }
 
     @Override
     public int remove(Category item) {
-        return -1;
+        int result = 0;
+        if (item.getId() > 0) {
+            try (Connection connection = dataSource.getConnection();
+                 PreparedStatement statement = connection.prepareStatement("DELETE FROM CATEGORY WHERE ID=?")) {
+                statement.setInt(1, item.getId());
+                result = statement.executeUpdate();
+            } catch (SQLException e) {
+                LOGGER.error("Exception while connecting to DB in method remove Category: " + item + " " + e);
+                throw new RuntimeException(e);
+            }
+        }
+        return result;
+
     }
 
     @Override
     public int update(Category item) {
-        return -1;
+        int result = 0;
+        if (item.getId() > 0) {
+            try (Connection connection = dataSource.getConnection();
+                 PreparedStatement statement = connection.prepareStatement("UPDATE CATEGORY SET NAME=? WHERE ID=?")) {
+                statement.setString(1, item.getName());
+                statement.setInt(2, item.getId());
+                result = statement.executeUpdate();
+            } catch (SQLException e) {
+                LOGGER.error("Exception while connecting to DB in method update Category: " + item + e);
+                throw new RuntimeException(e);
+            }
+        }
+        return result;
     }
 
     @Override
     public List<Category> getAll() {
-        return null;
+        List<Category> result = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM CATEGORY");
+
+            while (resultSet.next()) {
+                Category category = extractCategory(resultSet);
+                result.add(category);
+            }
+        } catch (SQLException e) {
+            LOGGER.error("Exception while connecting to DB in method getAllCategories: " + e);
+            throw new RuntimeException(e);
+        }
+        return result;
+
+    }
+    private Category extractCategory(ResultSet set) throws SQLException {
+        Category category = new Category();
+        category.setId(set.getInt("ID"));
+        category.setName(set.getString("NAME").trim());
+        return category;
     }
 
     @Override
     public Category findCategoryById(int id) {
-        return null;
+        Category category = new Category();
+        if (id > 0) {
+            try (Connection connection = dataSource.getConnection();
+                 PreparedStatement statement = connection.prepareStatement("SELECT * FROM CATEGORY WHERE ID=?")) {
+                statement.setInt(1, id);
+                ResultSet resultSet = statement.executeQuery();
+                if(resultSet.next()) {
+                    category = extractCategory(resultSet);
+                }
+            } catch (SQLException e) {
+                LOGGER.error("Exception while connecting to DB in method findCategoryByID: " + e);
+                throw new RuntimeException(e);
+            }
+
+        }
+        return category;
     }
 }
