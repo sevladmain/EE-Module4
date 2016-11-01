c660d05e0717cefc9aa805e15f586684e6646fbb
sevladmain <sevlad.main@gmail.com>
2016-11-01 23:18:24 +0200

    module 62 add IngredientList

--- module62/src/main/java/com/goit/homeworks/restaurant/dao/jdbc/JdbcIngredientListDao.java
+++ module62/src/main/java/com/goit/homeworks/restaurant/dao/jdbc/JdbcIngredientListDao.java
@@ -1,30 +1,95 @@
 package com.goit.homeworks.restaurant.dao.jdbc;
 
 import com.goit.homeworks.restaurant.dao.IngredientListDao;
+import org.apache.log4j.Logger;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by SeVlad on 31.10.2016.
  */
 public class JdbcIngredientListDao implements IngredientListDao {
+    private DataSource dataSource;
+    private static final Logger LOGGER = Logger.getLogger(JdbcIngredientListDao.class);
+
+    public JdbcIngredientListDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public int addIngredientToDish(int ingredientId, int dishId) {
-        return 0;
+        int result = 0;
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("INSERT INTO INGREDIENTLIST (ID_INGREDIENT, ID_DISH)  VALUES (?,?)")) {
+            statement.setInt(1, ingredientId);
+            statement.setInt(2, dishId);
+            result = statement.executeUpdate();
+        } catch (SQLException e) {
+            LOGGER.error("Exception while connecting to DB in method addIngredientToDish: " + e);
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
     @Override
     public int removeIngredientFromDish(int ingredientId, int dishId) {
-        return 0;
+        int result = 0;
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("DELETE FROM INGREDIENTLIST WHERE ID_INGREDIENT=? AND ID_DISH=?")) {
+            statement.setInt(1, ingredientId);
+            statement.setInt(2, dishId);
+            result = statement.executeUpdate();
+        } catch (SQLException e) {
+            LOGGER.error("Exception while connecting to DB in method removeIngredientFromDish: " + e);
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
     @Override
     public boolean isIngredientFromDish(int ingredientId, int dishId) {
-        return false;
+        boolean result = false;
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM INGREDIENTLIST WHERE ID_INGREDIENT=? AND ID_DISH=?")) {
+            statement.setInt(1, ingredientId);
+            statement.setInt(2, dishId);
+            ResultSet set = statement.executeQuery();
+            result = set.next();
+        } catch (SQLException e) {
+            LOGGER.error("Exception while connecting to DB in method isIngredientFromDish: " + e);
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
     @Override
     public List<Integer> getAllIngredientsIds(int dishId) {
-        return null;
+        List<Integer> result = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM INGREDIENTLIST WHERE ID_DISH=?")) {
+            statement.setInt(1, dishId);
+            ResultSet set = statement.executeQuery();
+            while (set.next()){
+                result.add(set.getInt("ID_INGREDIENT"));
+            }
+        } catch (SQLException e) {
+            LOGGER.error("Exception while connecting to DB in method getAllIngredientsIds: " + e);
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 }
---
+++ module62/src/test/java/com/goit/homeworks/restaurant/dao/jdbc/JdbcIngredientListDaoTest.java
@@ -0,0 +1,82 @@
+package com.goit.homeworks.restaurant.dao.jdbc;
+
+import com.goit.homeworks.restaurant.dao.IngredientListDao;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * Created by SeVlad on 01.11.2016.
+ */
+public class JdbcIngredientListDaoTest {
+    private EmbeddedDatabase db;
+    private JdbcTemplate jdbcTemplate;
+
+    private final static String SQL_SELECT_STATEMENT = "SELECT * FROM INGREDIENTLIST";
+    private IngredientListDao dao;
+    private int dishId;
+    private int existingIngrId1;
+    private int existingIngrId2;
+    private int newIngrId;
+
+    @Before
+    public void setUp() {
+        db = new EmbeddedDatabaseBuilder()
+                .addScript("scheme.sql")
+                .build();
+        jdbcTemplate = new JdbcTemplate(db);
+        dao = new JdbcIngredientListDao(db);
+        dishId = 1;
+        existingIngrId1 = 1;
+        existingIngrId2 = 2;
+        newIngrId = 3;
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        db.shutdown();
+    }
+
+    @Test
+    public void addIngredientToDishTest() {
+        assertThat(dao.addIngredientToDish(newIngrId, dishId), equalTo(1));
+        List<Map<String, Object>> rows = jdbcTemplate.queryForList("SELECT * FROM INGREDIENTLIST WHERE ID_DISH=" + dishId + " AND ID_INGREDIENT=" + newIngrId);
+        assertThat("Row not inserted or not exists", rows, hasSize(1));
+
+        assertThat(((Integer) rows.get(0).get("ID_DISH")), equalTo(dishId));
+        assertThat(((Integer) rows.get(0).get("ID_INGREDIENT")), equalTo(newIngrId));
+
+    }
+
+    @Test
+    public void removeIngredientFromDishTest() {
+        assertThat(dao.removeIngredientFromDish(existingIngrId1, dishId), equalTo(1));
+        List<Map<String, Object>> rows = jdbcTemplate.queryForList("SELECT * FROM INGREDIENTLIST WHERE ID_DISH=" + dishId + " AND ID_INGREDIENT=" + existingIngrId1);
+        assertThat("Row not inserted or not exists", rows, hasSize(0));
+    }
+
+    @Test
+    public void isIngredientFromDishTest() {
+        assertThat(dao.isIngredientFromDish(existingIngrId1, dishId), is(true));
+        assertThat(dao.isIngredientFromDish(existingIngrId2, dishId), is(true));
+        assertThat(dao.isIngredientFromDish(newIngrId, dishId), is(false));
+    }
+
+    @Test
+    public void getAllIngredientsIdsTest() {
+        List<Integer> list = dao.getAllIngredientsIds(dishId);
+        assertThat(list, hasSize(2));
+        assertThat(list, contains(existingIngrId1, existingIngrId2));
+    }
+
+}
