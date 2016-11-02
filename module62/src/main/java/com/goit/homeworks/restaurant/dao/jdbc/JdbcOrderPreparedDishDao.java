1e04b1cae26ec53c7b9d5f6ae25cf119a55ce3d7
sevladmain <sevlad.main@gmail.com>
2016-11-02 20:52:11 +0200

    module 62 add tests for OrderPreparedDish

--- module62/src/main/java/com/goit/homeworks/restaurant/dao/jdbc/JdbcOrderPreparedDishDao.java
+++ module62/src/main/java/com/goit/homeworks/restaurant/dao/jdbc/JdbcOrderPreparedDishDao.java
@@ -1,13 +1,31 @@
 package com.goit.homeworks.restaurant.dao.jdbc;
 
 import com.goit.homeworks.restaurant.dao.OrderPreparedDishDao;
+import org.apache.log4j.Logger;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 /**
  * Created by SeVlad on 31.10.2016.
  */
 public class JdbcOrderPreparedDishDao implements OrderPreparedDishDao {
+    private DataSource dataSource;
+    private static final Logger LOGGER = Logger.getLogger(JdbcOrderPreparedDishDao.class);
+
+    public JdbcOrderPreparedDishDao(DataSource dataSource) {
+
+        this.dataSource = dataSource;
+    }
+
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public int addPreparedDishToOrder(int preparedDishId, int orderId) {
         return 0;
