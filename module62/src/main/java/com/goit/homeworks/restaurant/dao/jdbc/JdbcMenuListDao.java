fcc87a985cb72bd98de82d38a91e6c2c020bf4d1
sevladmain <sevlad.main@gmail.com>
2016-11-02 20:42:20 +0200

    module 62 add MenuList tested

--- module62/src/main/java/com/goit/homeworks/restaurant/dao/jdbc/JdbcMenuListDao.java
+++ module62/src/main/java/com/goit/homeworks/restaurant/dao/jdbc/JdbcMenuListDao.java
@@ -4,6 +4,11 @@ import com.goit.homeworks.restaurant.dao.MenuListDao;
 import org.apache.log4j.Logger;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,21 +32,64 @@ public class JdbcMenuListDao implements MenuListDao {
 
     @Override
     public int addDishToMenu(int dishId, int menuId) {
-        return 0;
+        int result = 0;
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("INSERT INTO MENULIST (ID_MENU, ID_DISH)  VALUES (?,?)")) {
+            statement.setInt(1, menuId);
+            statement.setInt(2, dishId);
+            result = statement.executeUpdate();
+        } catch (SQLException e) {
+            LOGGER.error("Exception while connecting to DB in method addDishToMenu: " + e);
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
     @Override
     public int removeDishFromMenu(int dishId, int menuId) {
-        return 0;
+        int result = 0;
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("DELETE FROM MENULIST WHERE ID_MENU=? AND ID_DISH=?")) {
+            statement.setInt(1, menuId);
+            statement.setInt(2, dishId);
+            result = statement.executeUpdate();
+        } catch (SQLException e) {
+            LOGGER.error("Exception while connecting to DB in method removeDishFromMenu: " + e);
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
     @Override
     public boolean isDishFromMenu(int dishId, int menuId) {
-        return false;
+        boolean result = false;
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM MENULIST WHERE ID_MENU=? AND ID_DISH=?")) {
+            statement.setInt(1, menuId);
+            statement.setInt(2, dishId);
+            ResultSet set = statement.executeQuery();
+            result = set.next();
+        } catch (SQLException e) {
+            LOGGER.error("Exception while connecting to DB in method isDishFromMenu: " + e);
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
     @Override
     public List<Integer> getAllDishes(int menuId) {
-        return null;
+        List<Integer> result = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM MENULIST WHERE ID_MENU=?")) {
+            statement.setInt(1, menuId);
+            ResultSet set = statement.executeQuery();
+            while (set.next()){
+                result.add(set.getInt("ID_DISH"));
+            }
+        } catch (SQLException e) {
+            LOGGER.error("Exception while connecting to DB in method getAllDishes: " + e);
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 }
