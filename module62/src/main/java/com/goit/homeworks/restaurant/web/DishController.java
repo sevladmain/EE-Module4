24a7e9dd8847338dcac02acd219e73c8d755b0d7
sevladmain <sevlad.main@gmail.com>
2016-11-10 21:35:46 +0200

    module 62 delete dish

--- module62/src/main/java/com/goit/homeworks/restaurant/web/DishController.java
+++ module62/src/main/java/com/goit/homeworks/restaurant/web/DishController.java
@@ -1,12 +1,16 @@
 package com.goit.homeworks.restaurant.web;
 
+import com.goit.homeworks.restaurant.core.Dish;
+import com.goit.homeworks.restaurant.core.Employee;
 import com.goit.homeworks.restaurant.services.DishService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Map;
 
@@ -34,5 +38,18 @@ public class DishController {
         model.put("dishes", dishService.getAllDishes());
         return "app.dishes";
     }
+
+    @RequestMapping(value = "/dish/{id}/delete", method = RequestMethod.POST)
+    public String deleteDish(@PathVariable("id") int id, final RedirectAttributes redirectAttributes) {
+
+        LOGGER.debug("deleteDish() is executed!");
+        Dish dish = dishService.getDishById(id);
+        dishService.deleteDish(dish);
+        redirectAttributes.addFlashAttribute("css", "success");
+        redirectAttributes.addFlashAttribute("msg", "Страва " + dish.getName()
+                + " видалена з бази даних");
+        return "redirect:/dish/all";
+    }
+
 
 }
