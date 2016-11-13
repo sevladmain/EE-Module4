5dfcd381cd5c6d51d41f90ef29d5905b2f748bde
sevladmain <sevlad.main@gmail.com>
2016-11-13 13:50:15 +0200

    module 62 - delete order view

--- module62/src/main/java/com/goit/homeworks/restaurant/web/OrderController.java
+++ module62/src/main/java/com/goit/homeworks/restaurant/web/OrderController.java
@@ -1,13 +1,17 @@
 package com.goit.homeworks.restaurant.web;
 
+import com.goit.homeworks.restaurant.core.Menu;
+import com.goit.homeworks.restaurant.core.Order;
 import com.goit.homeworks.restaurant.services.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Map;
 
@@ -26,7 +30,7 @@ public class OrderController {
     }
 
     @RequestMapping(value = "/order/allOpen", method = RequestMethod.GET)
-    public String getAllOpenOrders(Model model){
+    public String getAllOpenOrders(Model model) {
         LOGGER.debug("getAllOpenOrders() is executed!");
         model.addAttribute("orders", orderService.getAllOpenOrders());
         model.addAttribute("isClosed", "OPEN");
@@ -35,12 +39,24 @@ public class OrderController {
     }
 
     @RequestMapping(value = "/order/allClosed", method = RequestMethod.GET)
-    public String getAllClosedOrders(Model model){
+    public String getAllClosedOrders(Model model) {
         LOGGER.debug("getAllClosedOrders() is executed!");
         model.addAttribute("orders", orderService.getAllClosedOrders());
         model.addAttribute("isClosed", "CLOSED");
 
         return "app.orders";
     }
+
+    @RequestMapping(value = "/order/{id}/delete", method = RequestMethod.POST)
+    public String deleteOrder(@PathVariable("id") int id, final RedirectAttributes redirectAttributes) {
+        LOGGER.debug("deleteOrder() is executed!");
+        Order order = orderService.getOrderById(id);
+        orderService.deleteOrder(order);
+        redirectAttributes.addFlashAttribute("css", "success");
+        redirectAttributes.addFlashAttribute("msg", "Замовлення #" + order.getId() + " успішно видалено");
+        String redirect = order.isOpen() ? "allOpen" : "allClosed";
+        return "redirect:/order/" + redirect;
+    }
+
 
 }
